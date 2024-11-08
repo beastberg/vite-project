ee798a2ef834f935ed6e3a7eb2cdc9a0e01e7a23
Ankit kumar malik <light123321@grr.la>
2024-11-08 23:19:31 +0530

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import Task from "./Task";
+import "./Home.css";
+
+const Home = () => {
+
+    const [task, setTask] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const submitHandler=(e)=>{
+        e.preventDefault();//this doesn't reload page while submitting
+        setTask([...task,{ //this is the spread operator and this will add new object to end of list
+            title,
+            description
+        }]);
+        setDescription("");
+        setTitle("");//this will empty the variable part
+
+    }
+    const deleteTask = (index)=>{
+        const filterArr = task.filter((_,i)=>{
+            return i!==index;
+        })
+        console.log(filterArr);
+        setTask(filterArr);
+    }
+    useEffect(()=>{
+      task.map((index,item)=>{
+        console.log("added",task)
+      })
+    },[task])
+    return (
+        <div className="container">
+            <h1>Daily Goal</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    placeholder='tilte'
+                    value={title}
+                    onChange={(e)=>{
+                        setTitle(e.target.value)
+                    }}
+                    
+                />
+
+                <textarea 
+                    placeholder="Description"
+                    value={description}
+                    onChange={(e)=>setDescription(e.target.value)} //this will listen to change on screen 
+                ></textarea>
+                <button type="submit">Add</button>
+            </form>
+
+            {/**introduce javascript here */}
+            {task.map((item,index)=>(
+                <Task 
+                key={index}
+                title={item.title}
+                description={item.description}
+                deleteTask={deleteTask}
+                index={index}
+                />
+            ))}
+        </div>
+    )
+} 
+
+export default Home
